5922e8326ab0a5f8209310406b669c013158fec2
Milor123 <kora_bohorquez@hotmail.com>
2016-03-10 16:31:13 -0500

    docstrings working

--- Appl.py
+++ Appl.py
@@ -21,29 +21,73 @@ from functions.decorators import *
 
 
 class Myldap(object):
+    """
+    firts o all, this module is specialy created for active directory working of the university
+    "Instituto Tolimence de Educacion Superior" (ITFIP), where our objetive is easly to
+    centralization of data through ldap protocol.
+
+    This module establish a connection to active directory of windows server, by means of a user of domain,
+    on top that allow data query, and other functions sush as, add data and modify, but are still experimentals.
+    """
 
     @requiere(str, str, str)
-    def __init__(self, ip, dn, __password, port='389'):
+    def __init__(self, ip, dn, _password, port='389'):
         # the Distinguished Names (dn) must contain a user and his route, it should have read permission,
         # however read and write should be the best option,
         # because as opposed to read only cant add user or read another directories
         self.ip = ip
         self.dn = dn
-        self.__password = __password
+        self._password = _password
         self.port = port
         self.domain = to_domain(dn)
         self.attrib_toshow = []
-        self.__connect()
+        self._connect()
 
-    def __connect(self):
+    def _connect(self):
         try:
             self.conn=ldap.initialize("ldap://"+self.ip+":"+self.port)
-            self.conn.simple_bind(self.dn, self.__password)  # simple bind changed
+            self.conn.simple_bind(self.dn, self._password)  # simple bind changed
             print self.conn.whoami_s()
         except:
             print('Error al conectar a ldap')
 
     def ldapsearch(self, attrib_forsearch, attrib_toshow, **kwargs):
+        """
+        ldapsearch() -> list
+
+        This is a longer explanation, which may include math with latex syntax
+        Then, you need to provide optional subsection in this order (just to be
+        consistent and have a uniform documentation. Nothing prevent you to
+        switch the order):
+
+        Args:
+            attrib_forsearch (str): its composed by two importants parts:
+                firts, the attribute of domain, then that value for data search
+            attrib_toshow (list): use attributes with comma separate what you need get
+            **kwargs: it is used for change default domain of search,
+                you can use url as domain or distinguishedName, use domain='dn_or_url'
+
+
+        Returns:
+            list from getsearch()
+
+        Raises:
+            NameError: In case that you kwarg domain is corrupted
+
+        .. note:: search_this and show_this module could help you``
+        .. seealso:: https://msdn.microsoft.com/en-us/library/windows/desktop/ms675090(v=vs.85).aspx``
+
+        Example:
+            without help modules
+            >>> ldapsearch('mail=mailfor@search.com', ['distinguishedName'])
+            [['distinguishedName', ['CN=name_user,CN=Users,DC=owner,DC=local']]]
+
+            with show_this and search_this functions
+            >>> ldapsearch(search_by_mail('mailfor@search.com'), show_dn())
+            [['distinguishedName', ['CN=name_user,CN=Users,DC=owner,DC=local']]]
+
+        """
+
         self.attrib_toshow = attrib_toshow
         try:
             self.conn.protocol_version = ldap.VERSION3
@@ -56,7 +100,7 @@ class Myldap(object):
             if 'domain' in kwargs:
                 if is_domain_dn(kwargs['domain']):  # verify if is a valid domain
                     self.domain = kwargs['domain']
-                elif is_domain_url(kwargs['domain']):
+                elif is_domain_url(kwargs['domain']):  # verify if is a valid url domain
                     self.domain = to_distinguished_name(kwargs['domain'])  # convert url domain to dn
                 else:
                     raise NameError('Has escrito un dominio invalido, asegurate de no tener espacios,'
@@ -112,7 +156,7 @@ class Myldap(object):
 
 
 Nop = Myldap('192.168.0.23', 'cn=administradortest,cn=Users,dc=owner,dc=local','123456789Xx')
-Nop.ldapsearch(search_by_mail('a@a.a'), show_name())
+Nop.ldapsearch(search_by_mail('a@a.a'), show_dn())
 
 
 
